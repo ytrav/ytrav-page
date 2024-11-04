b19eedf0ceccf04d5e311f07c6691934a646ce4a
ytrav <withertrav394@gmail.com>
2024-11-04 14:53:43 +0100

    fixed translate functionality

--- api/translate.js
+++ api/translate.js
@@ -1,6 +1,7 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.local' })
-const { Translate } = require('@google-cloud/translate').v2
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+import { Translate } from '@google-cloud/translate/build/src/v2/index.js';
+
 /* eslint-env node */
 // Imports the Google Cloud client library
 
@@ -9,19 +10,19 @@ const { Translate } = require('@google-cloud/translate').v2
 export default async function handler(req, res) {
   const translate = new Translate({
     key: process.env.TRANSLATION_API_KEY
-  })
+  });
   try {
-    const { text } = req.body
-    const target = 'en'
-    let [translations] = await translate.translate(text, target)
-    translations = Array.isArray(translations) ? translations : [translations]
+    const { text } = req.body;
+    const target = 'en';
+    let [translations] = await translate.translate(text, target);
+    translations = Array.isArray(translations) ? translations : [translations];
     // console.log('text:', text)
     // console.log('target:', target)
     // console.log('key:', process.env.TRANSLATION_API_KEY)
 
-    res.status(200).json({ success: true, translations })
+    res.status(200).json({ success: true, translations });
   } catch (error) {
-    console.error('Error translating text:', error)
-    res.status(500).json({ success: false, error: 'Failed to translate text' })
+    console.error('Error translating text:', error);
+    res.status(500).json({ success: false, error: 'Failed to translate text' });
   }
 }
