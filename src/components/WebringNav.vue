ace449ed82c2260d77aabba283551a2538129788
ytrav <withertrav394@gmail.com>
2025-07-28 18:35:05 +0200

    make webring nav dynamic, add 88x31 button

---
+++ src/components/WebringNav.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+    props: {
+        webring: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        prettyLink() {
+            return this.webring.url.replace(/https?:\/\//, '').replace(/\/$/, '');
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="webring-nav">
+        <a title="Go to the previous website in the ring" class="previous"
+            :href="`${webring.url}/${webring.mySlug}/previous`"><span class="arrow">←</span></a>
+        <a :title="`${webring.name} main page`" class="ring" :href="webring.url">
+            <div class="text">{{ webring.name }} <span>{{ prettyLink }}</span></div>
+        </a>
+        <a title="Go to the next website in the ring" class="next" :href="`${webring.url}/${webring.mySlug}/next`"><span
+                class="arrow">→</span></a>
+    </div>
+</template>
